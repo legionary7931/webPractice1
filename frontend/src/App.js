41b3516e2910f74a32355d443832d4a208197744
jayLrei <thfpdhk1@gmail.com>
2023-03-17 18:47:39 +0900

    feat : page 나누고 LoginPage만들었음.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
+import LogoutPage from './pages/LogoutPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
-    <div class="container auth">
-        <a class="logo">MEMOPAD</a>
-        <div class="card">
-            <div class="header blue white-text center">
-                <div class="card-content">LOGIN</div>
-            </div>
-            <div class="card-content">
-                <div class="row">
-                    
-                    <div class="input-field col s12 username">
-                        <label>Username</label>
-                        <input
-                        name="username"
-                        type="text"
-                        class="validate"/>
-                    </div>
-                    <div class="input-field col s12">
-                        <label>Password</label>
-                        <input
-                        name="password"
-                        type="password"
-                        class="validate"/>
-                    </div>
-                    <a class="waves-effect waves-light btn">SUBMIT</a>
-                </div>
-            </div>
-            
-            
-            <div class="footer">
-                <div class="card-content">
-                    <div class="right" >
-                        New Here? <a>Create an account</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/findID" element={<LogoutPage/>}/>
+        <Route path='/signUpPage' element={<SignUpPage/>}/>
+    </Routes>
   );
 }
 
