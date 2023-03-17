bc776e4d46372dfc8e38ff27a87afe0f22796810
jayLrei <thfpdhk1@gmail.com>
2023-03-17 21:50:45 +0900

    feat : 폴더 구조 편집 중

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import Header from './Header';
-import Authentication from './components/Authentication';
-// Router
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-// Container Components
-import { App, Home, Login, Register } from 'containers';
-// Redux
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from 'reducers';
-import thunk from 'redux-thunk';
 
-const store = createStore(reducers, applyMiddleware(thunk));
-
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -30,5 +19,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-export { Header, Authentication };
 
