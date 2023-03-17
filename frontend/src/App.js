939d9c3a174061a2121a47b553300cde7431ee81
jayLrei <thfpdhk1@gmail.com>
2023-03-17 20:34:31 +0900

    feat : redux 를 src/index.js 에 넣는 중임.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage';
 import LogoutPage from './pages/LogoutPage';
 import SignUpPage from './pages/SignUpPage';
 
 function App() {
+  let navigate = useNavigate()
   return (
-    <Routes>
+    <div>
+      <button onClick={()=>{ navigate('/login') }}>로그인</button>
+      <button onClick={()=>{ navigate('/logout') }}>로그아웃</button>
+      <button onClick={()=>{ navigate('/signuppage') }}>회원가입</button>
+      
+      <Routes>
         <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/findID" element={<LogoutPage/>}/>
-        <Route path='/signUpPage' element={<SignUpPage/>}/>
-    </Routes>
+        <Route path="/logout" element={<LogoutPage/>}/>
+        <Route path='/signuppage' element={<SignUpPage/>}/>
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+import Authentication from './components/Authentication';
+// Router
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+// Container Components
+import { App, Home, Login, Register } from 'containers';
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,3 +30,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export { Header, Authentication };
+
